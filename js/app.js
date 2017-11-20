49858ff0c1bdd80d527195d5e389e8f1e113c29f
Lauraisa <galindolaura.isabel@gmail.com>
2017-11-19 23:26:12 -0500

    Subiendo dom-js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+elemento = document.createElement('div');
+elemento.id='Caja';
+document.body.appendChild(elemento).section;
+elemento.addEventListener('keyDown',function(){
+ var newDiv = document.getElementById('caja');
+ 
+})
+
